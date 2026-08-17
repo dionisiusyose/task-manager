a5d8aa17e6614cdd76cddcacf0d899d25c43d284
immortalsaurus <dionisiusyose11@gmail.com>
2026-08-17 08:57:13 +0700

    Add service pattern

--- src/main/java/com/dionisius/taskmanager/controller/TaskController.java
+++ src/main/java/com/dionisius/taskmanager/controller/TaskController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dionisius.taskmanager.entity.Task;
-import com.dionisius.taskmanager.repository.TaskRepository;
+import com.dionisius.taskmanager.service.TaskService;
 
 import java.util.List;
 
@@ -27,69 +27,54 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/api/v1/tasks")
 public class TaskController {
 
-    private final TaskRepository taskRepository;
+    private final TaskService taskService;
 
     // Constructor Injection
-    public TaskController(TaskRepository taskRepository){
-        this.taskRepository = taskRepository;
+    public TaskController(TaskService taskService){
+        this.taskService = taskService;
     }
 
     @GetMapping
     public List<Task> getAllTasks() {
-        return taskRepository.findAll();
+        return taskService.getAllTasks();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
-        return taskRepository.findById(id)
+        return taskService.getTaskById(id)
             .map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
 
     @PostMapping
     public ResponseEntity<Task> createTask(@RequestBody Task task) {
-        Task savedTask = taskRepository.save(task);
+        Task savedTask = taskService.createTask(task);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedTask);
     }
     
     @PutMapping("/{id}")
     public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
-        return taskRepository.findById(id)
-            .map(task -> {
-                task.setTitle(updatedTask.getTitle());
-                task.setDescription(updatedTask.getDescription());
-                task.setCompleted(updatedTask.getCompleted());
-                Task savedTask = taskRepository.save(task);
-                return ResponseEntity.ok(savedTask);
-            })
-        .orElse(ResponseEntity.notFound().build());
+        return taskService.updateTask(id, updatedTask)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
 
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTask(@PathVariable Long id){
-        return taskRepository.findById(id)
-            .map(task -> {
-                taskRepository.delete(task);
-                return ResponseEntity.ok().<Void>build();
-            })
-            .orElse(ResponseEntity.notFound().build());
+        return taskService.deleteTask(id) ?
+            ResponseEntity.ok().build() :
+            ResponseEntity.notFound().build();
     }
 
     @GetMapping("/completed/{status}")
     public List<Task> getTaskByCompletions(@PathVariable boolean status) {
-        return taskRepository.findByCompleted(status);
+        return taskService.getTasksByCompletionStatus(status);
     }
     
     @GetMapping("/search")
     public  List<Task>  searchTasksByTitle(@RequestParam String title) {
-        return taskRepository.findByTitleContainingIgnoreCase(title);
-    }
-    
-
-    @GetMapping("/completioni/{status}")
-    public List<Task> getTaskCompleted(@PathVariable boolean status) {
-        return taskRepository.findTasksByCompletionStatus(status);
+        return taskService.searchTasksByTitle(title);
     }
     
 }
